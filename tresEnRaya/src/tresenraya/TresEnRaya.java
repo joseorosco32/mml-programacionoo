9aea4422268c22a2339464c892d28c9ab25a9258
joseorosco32 <jose32orosco@gmail.com>
2016-10-04 11:41:18 -0500

    Update TresEnRaya.java

--- tresEnRaya/src/tresenraya/TresEnRaya.java
+++ tresEnRaya/src/tresenraya/TresEnRaya.java
@@ -23,6 +23,7 @@ public class TresEnRaya {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int a=20;
         // TODO code application logic here
     }
     
